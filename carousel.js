7bead4db3e11736d6b34a673094554d09e81e478
Velko <98957035+VelkoMijalkovic@users.noreply.github.com>
2022-06-28 22:47:28 +0200

    changed getting access to the api to a fetch

--- carousel.js
+++ carousel.js
@@ -1,7 +1,7 @@
 const sliders = document.querySelector(".carouselbox");
 var scrollPerClick;
 var imagePadding = 20;
-
+let result;
 showMovieData();
 
 var scrollAmount = 0;
@@ -27,18 +27,30 @@ function sliderScrollRight() {
 }
 async function showMovieData() {
   const API_KEY = "856bf84a340e6a4e0b3c55c48d17ae07";
-
-  var result = await axios.get(
+ 
+  fetch(
     "https://api.themoviedb.org/3/discover/movie?api_key=" +
       API_KEY +
       "&sort_by=popularity.desc"
-  );
-  result = result.data.results;
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("error");
+      }
+    })
+    .then((data) => {
+      result = data;
+      console.log(result);
+    
+  result = result.results;
   result.map(function (x, index) {
     sliders.insertAdjacentHTML(
       "beforeend",
       `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${x.poster_path}"/>`
     );
+  });
   });
   scrollPerClick = 500;
 }
