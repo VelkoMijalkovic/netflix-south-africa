c03065087f24b299326956028a3a403f6ee13f95
Velko <98957035+VelkoMijalkovic@users.noreply.github.com>
2022-06-30 19:17:10 +0200

    error checking in progress
    removed unnecessary css, error checking in progress for log in

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,17 @@
 const app = Vue.createApp({
     data() {
       return {
+        email : '',
+        password: '',
        //check for admin admin to access
       }
     },
     methods: {
-      
-    },
+      check() {
+        if ((this.email === 'Admin@admin') && (this.password === 'Admin')){
+          
+        }
+      }
+    }
     
   })
