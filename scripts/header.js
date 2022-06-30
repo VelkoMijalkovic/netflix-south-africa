61969cdb938066749e1998e8e7ff9803d23fec07
Velko <98957035+VelkoMijalkovic@users.noreply.github.com>
2022-06-30 16:50:44 +0200

    Add to list implementation needed
    Flow added, need check on login page for admin admin, fetch requests have been broken down and more will be added that interact with the api

--- scripts/header.js
+++ scripts/header.js
@@ -3,10 +3,10 @@ const app = Vue.createApp({
     return {
       menu: ["Home", "Tv Shows", "Movies", "Latest", "My List"],
       isActive: false,
-      scrollPerClick: 500,
+      scrollPerClick: 250,
       imagePadding: 20,
       scrollAmount: 0,
-      result: {},
+      
     };
   },
   methods: {
@@ -27,8 +27,7 @@ const app = Vue.createApp({
         })
         .then((data) => {
           result = data;
-          console.log(result);
-          const sliders = document.getElementById("latest");
+          const sliders = document.getElementById("Latest");
           result = result.results;
           result.map(function (x, index) {
             sliders.insertAdjacentHTML(
@@ -39,8 +38,176 @@ const app = Vue.createApp({
         });
       
     },
-    sliderScrollLeft() {
-      let sliders = document.querySelector(".carouselbox");
+    showLatestTV() {
+      const API_KEY = "856bf84a340e6a4e0b3c55c48d17ae07";
+
+      fetch(
+        "https://api.themoviedb.org/3/discover/tv?api_key=" +
+          API_KEY +
+          "&with_networks=213"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log("error");
+          }
+        })
+        .then((data) => {
+          result = data;
+          const sliders = document.getElementById("LatestTV");
+          result = result.results;
+          result.map(function (x, index) {
+            sliders.insertAdjacentHTML(
+              "beforeend",
+              `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${x.poster_path}"/>`
+            );
+          });
+        });
+      
+    },
+    showComedy() {
+      const API_KEY = "856bf84a340e6a4e0b3c55c48d17ae07";
+
+      fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=" +
+          API_KEY +
+          "&with_genres=35"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log("error");
+          }
+        })
+        .then((data) => {
+          result = data;
+          const sliders = document.getElementById("Comedy");
+          result = result.results;
+          result.map(function (x, index) {
+            sliders.insertAdjacentHTML(
+              "beforeend",
+              `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${x.poster_path}"/>`
+            );
+          });
+        });
+      
+    },
+    showKids() {
+      const API_KEY = "856bf84a340e6a4e0b3c55c48d17ae07";
+
+      fetch(
+        "https://api.themoviedb.org/3/discover/tv?api_key=" +
+          API_KEY +
+          "&with_genres=10762"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log("error");
+          }
+        })
+        .then((data) => {
+          result = data;
+          const sliders = document.getElementById("Kids");
+          result = result.results;
+          result.map(function (x, index) {
+            sliders.insertAdjacentHTML(
+              "beforeend",
+              `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${x.poster_path}"/>`
+            );
+          });
+        });
+      
+    },
+    showHorror() {
+      const API_KEY = "856bf84a340e6a4e0b3c55c48d17ae07";
+
+      fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=" +
+          API_KEY +
+          "&with_genres=27"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log("error");
+          }
+        })
+        .then((data) => {
+          result = data;
+          const sliders = document.getElementById("Horror");
+          result = result.results;
+          result.map(function (x, index) {
+            sliders.insertAdjacentHTML(
+              "beforeend",
+              `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${x.poster_path}"/>`
+            );
+          });
+        });
+      
+    },
+    showDocumentary() {
+      const API_KEY = "856bf84a340e6a4e0b3c55c48d17ae07";
+
+      fetch(
+        "https://api.themoviedb.org/3/discover/tv?api_key=" +
+          API_KEY +
+          "&with_genres=99"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log("error");
+          }
+        })
+        .then((data) => {
+          result = data;
+          const sliders = document.getElementById("Documentary");
+          result = result.results;
+          result.map(function (x, index) {
+            sliders.insertAdjacentHTML(
+              "beforeend",
+              `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${x.poster_path}"/>`
+            );
+          });
+        });
+      
+    },
+    showMusical() {
+      const API_KEY = "856bf84a340e6a4e0b3c55c48d17ae07";
+
+      fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=" +
+          API_KEY +
+          "&with_genres=10402"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log("error");
+          }
+        })
+        .then((data) => {
+          result = data;
+          const sliders = document.getElementById("Musical");
+          result = result.results;
+          result.map(function (x, index) {
+            sliders.insertAdjacentHTML(
+              "beforeend",
+              `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${x.poster_path}"/>`
+            );
+          });
+        });
+      
+    },
+    sliderScrollLeft(genre) {
+      let sliders = document.getElementById(genre);
       sliders.scrollTo({
         top: 0,
         left: (this.scrollAmount -= this.scrollPerClick),
@@ -50,8 +217,8 @@ const app = Vue.createApp({
         this.scrollAmount = 0;
       }
     },
-    sliderScrollRight() {
-      const sliders = document.querySelector(".carouselbox");
+    sliderScrollRight(genre) {
+      const sliders = document.getElementById(genre);
       if (this.scrollAmount <= sliders.scrollWidth - sliders.clientWidth) {
         sliders.scrollTo({
           top: 0,
@@ -63,7 +230,12 @@ const app = Vue.createApp({
   },
   created() {
     this.showLatest();
-    
+    this.showComedy();
+    this.showLatestTV();
+    this.showKids();
+    this.showHorror();
+    this.showDocumentary();
+    this.showMusical();
   },
 });
 
