f40a54249475f541c51e05bbadc505dea49c6fe5
Velko <98957035+VelkoMijalkovic@users.noreply.github.com>
2022-07-01 13:49:52 +0200

    error checking for admin done
    Pages are complete, still trying to complete my list for user, need to get data/result out of the promise, put into a new array then checked whenever a user clicks on a movie image

--- scripts/login.js
+++ scripts/login.js
@@ -3,14 +3,21 @@ const app = Vue.createApp({
       return {
         email : '',
         password: '',
+        isDisabled: false,
+        error: 'wrong username/password',
+        showError: false,
+
        //check for admin admin to access
       }
     },
     methods: {
-      check() {
-        if ((this.email === 'Admin@admin') && (this.password === 'Admin')){
-          
-        }
+     
+    },
+    computed: {
+      check : function(){
+        if ((this.email === 'admin@admin') && (this.password === 'admin')){
+         return this.isDisabled = true;
+          } 
       }
     }
     
